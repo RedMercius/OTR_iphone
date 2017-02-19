daa189b0af85275c630e195f5317edc96a6ede7b
Johnnie Ruffin <redmac@Johnnies-MacBook-Air.local>
2017-02-19 13:13:24 -0500

    View Swift files and radio titles added

---
+++ Johnnie's Old Time Radio/WesternViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WesternViewController.swift
+//  Johnnie's Old Time Radio
+//
+//  Created by Johnnie Ruffin on 2/19/17.
+//  Copyright © 2017 Johnnie Ruffin. All rights reserved.
+//
+
+import UIKit
+
+class WesternViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
